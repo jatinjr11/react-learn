bbea730e43f82d97843ea7c1dd8f5eaf46d62b52
Jatin <jatin333x@gmail.com>
2025-07-13 18:37:15 +0530

    dynamic vlaues

--- tha/lec-3 conditinals/src/App.jsx
+++ tha/lec-3 conditinals/src/App.jsx
@@ -4,30 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const name = "Queen of tears"
+  const rating = 8.2;
+  const summary = "Queen of Tears is a 2022 Indian Hindi-language romantic drama film directed by Rajat Kapoor and written by Kapoor and Anubhav Sinha. The film stars Ranbir Kapoor, Anushka Sharma, and Radhika Apte. The film was released on 18 November 2022 on Netflix.";
+  let age = 16;
+
+  const reutrnGenre = () => {
+    return "Hello Ji"
+  }
+
+  const canWatch = () => {
+    if (age >= 18 && age <= 25) return "Watch Now"
+    return "Not Avail"
+  }
 
   return (
     <>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <img src="queen.webp" alt="" width="40%" height="40%" />
+        </div>
+        <h2>Name: {name} </h2>
+        <h3>Rating: {rating}</h3>
+        <p>{summary}</p>
+        <p>Genre: {reutrnGenre()}</p>
+      <button>{canWatch()}</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+      {/* <button> {age>= 18 ? "Watch now" : "Not Avail"} </button> */}
     </>
   )
 }
