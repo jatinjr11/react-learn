01f4847f1dc3c9057137f81dfb8c82ec754edfbd
Jatin <jatin333x@gmail.com>
2025-07-26 19:08:54 +0530

    conditional rendering

--- reactLearn/my/lec-10-condi-rendering/src/App.jsx
+++ reactLearn/my/lec-10-condi-rendering/src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
 
 function App() {
 
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
   return (
     <>
-
+    <div>
+    { isLoggedIn ? <Login/> : <Logout/> }
+    </div>
     </>
   )
 }
