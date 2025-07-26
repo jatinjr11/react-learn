3413abf89c9af573c4a431569d68413b5bcfc747
Jatin <jatin333x@gmail.com>
2025-07-26 17:39:29 +0530

    child props

---
+++ reactLearn/my/lec-9-child-props/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    {/* <Card name="sachin">
+      <h1>I am Sachin</h1>
+      <p>I am from khandwa</p>
+      <p>I am a CSE Student</p>
+    </Card> */}
+
+    
+    </>
+  )
+}
+
+export default App
---
+++ reactLearn/my/lec-9-child-props/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <>
+    {props.children}
+    </>
+  )
+}
+
+export default Card
