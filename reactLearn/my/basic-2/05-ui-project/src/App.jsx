2e123ff373405a652d253f70d6dfc6c5c4b839dd
Jatin <jatin333x@gmail.com>
2025-10-20 15:39:43 +0530

    2nd ui project

---
+++ reactLearn/my/basic-2/05-ui-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Section1 from './components/Section1/Section1'
+import Page1Content from './components/Section1/Page1Content'
+
+const App = () => {
+  return (
+    <>
+    <div className='h-screen w-full'>
+    <Section1/>
+    <Page1Content/>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ reactLearn/my/basic-2/05-ui-project/src/components/Section1/LeftContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+
+const LeftContent = () => {
+  return (
+    <div className='h-full w-1/4 bg-blue-600'>
+        <div></div>
+        <div>
+        <i class="ri-arrow-right-up-line"></i>
+        </div>
+    </div>
+  )
+}
+
+export default LeftContent
