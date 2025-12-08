cbac6446e6dc4c7391416bd8efbce5bc0cc78e21
Jatin <jatin333x@gmail.com>
2025-12-08 12:29:34 +0530

    useEffect code

--- reactLearn/my/lec-15-useEffect/src/App.jsx
+++ reactLearn/my/lec-15-useEffect/src/App.jsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import LoggerComponent from './components/LoggerComponent';
-import DataFetcher from './components/DataFetcher';
+import LoggerComponent from './components/LoggerComponent'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [total,setTotal] = useState(1);
+  // const [count, setCount] = useState(0);
+  // const [total, setTotal] = useState(1);
 
   // first -> side effect function
   // second -> cleanup function
@@ -31,46 +28,48 @@ function App() {
   },[])
   */
 
+  /*
   // variation-3
-  // runs on first render and also when count changes
-  // useEffect(() => {
-  //   alert("i will run on first render and also when count changes")
-  // },[count])
+  useEffect(() => {
+    alert("i will run every time when count/total is updated.")
+  }, [count, total])
+  */
 
+  /*
   // variation-4
   // this time to add cleanup function
-  // useEffect(() => {
-  //   alert("i will run on first render and also when count changes")
-  //   return () => {
-  //     alert("count will unmounted from UI")
-  //   }
-  // }, [count])
+  useEffect(() => {
+    alert("i will run on first render and also when count changes")
+    return () => {
+      alert("count will unmounted from UI")
+    }
+  }, [count])
+  */
 
-  function handleClick(){
+  function handleClick() {
     setCount(count + 1)
   }
 
-  function handleClickTotal(){
+  function handleClickTotal() {
     setTotal(total + 1)
   }
 
   return (
-    // <>
-    // <button onClick={handleClick}>
-    //   Click Me
-    // </button>
-    // <br />
-    // Count is: {count}
-    // <br />
-    // <button onClick={handleClickTotal}>
-    //   Click Update
-    // </button>
-    // <br />
-    // total is: {total}
-    // </>
     <>
-    {/* <LoggerComponent/> */}
-    <DataFetcher/>
+      {/* <button onClick={handleClick}>
+        Click Me
+      </button>
+      <br />
+      Count is: {count}
+
+      <br />
+
+      <br />
+      <button onClick={handleClickTotal} >Update Total</button>
+      <br />
+      Total is: {total} */}
+
+      <LoggerComponent/>
     </>
   )
 }
