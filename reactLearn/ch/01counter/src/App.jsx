eeb7e4ed905e66264ceb321feee25a2a828929cd
Jatin <jatin333x@gmail.com>
2026-02-12 13:41:34 +0530

    counter update

--- reactLearn/ch/01counter/src/App.jsx
+++ reactLearn/ch/01counter/src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const addValue = () => {
+    if (count < 20) {
+      setCount(count + 1);
+    }
+  };
+
+  const removeValue = () => {
+    if(count > 0 ) setCount(count-1);
+  };
 
   return (
-    <h1>Hyyy</h1>
-  )
+    <>
+      <h1>Value : {count}</h1>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
 }
 
-export default App
+export default App;
